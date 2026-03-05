1a8fb50d065bdd10671f552c656ae8c48c6b8710
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-05 23:05:02 +0530

    Generate token has been created...

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const connectDB = require("./config/db");
+const connectDB = require("./src/config/db");
 
 const app = express();
 
