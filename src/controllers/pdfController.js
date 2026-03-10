384fea2e9ba04eeca2d74e325afebaa6d471a5a3
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-11 00:39:19 +0530

    Function to check if biodata available has been added...

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -3,3 +3,15 @@ const User = require("../models/User");
 const Biodata = require("../models/BioData");
 const { buildBiodataHTML } = require("../utils/pdfTemplate");
 const puppeteer = require("puppeteer");
+
+// --Generate & Download Pdf--
+const generatePDF = asyncHandler(async (req, res) => {
+  const biodata = await Biodata.findById(req.params.id);
+
+  if (!biodata) {
+    res.status(404);
+    throw new Error("Biodata not found");
+  }
+});
+
+module.exports = { generatePDF };
