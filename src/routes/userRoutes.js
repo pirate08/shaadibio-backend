a15a15123924d3578fedee5557d4f8f87491ea5e
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-17 19:36:30 +0530

    Added route and the api of update profile is working perfectly...

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { getUserProfile } = require("../controllers/userController");
+const {
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
+// --Get User Route--
 router.get("/get-profile", protect, getUserProfile);
+
+// --Update User Route--
+router.patch("/update-profile", protect, updateUserProfile);
 
 module.exports = router;
