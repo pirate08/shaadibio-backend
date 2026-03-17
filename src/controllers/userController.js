e0935eeb5cb91519f36c543c90e4aa149d77c8fc
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-17 18:34:49 +0530

    Get user profile user existence function has been created...

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,3 +2,13 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const Biodata = require("../models/BioData");
 const { cloudinary } = require("../config/cloudinary");
+
+// --Get User Profile--
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User account not found");
+  }
+});
