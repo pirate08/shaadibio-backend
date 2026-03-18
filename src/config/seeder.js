ee18e36963d905a1968dcc0e73caad0992f744f3
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-18 19:33:57 +0530

    Traditional demo template data has been added...

--- src/config/seeder.js
+++ src/config/seeder.js
@@ -2,3 +2,17 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const connectDB = require("./db");
 const Template = require("../models/Template");
+
+const templates = [
+  {
+    name: "traditional",
+    label: "Traditional",
+    description:
+      "Classic Indian biodata with elegant borders and formal layout",
+    category: "Traditional",
+    defaultFont: "georgia",
+    defaultColor: "#8B1A4A",
+    colorOptions: ["#8B1A4A", "#6B0F0F", "#1A3A8B", "#1A7A2E", "#8B6A1A"],
+    isActive: true,
+  },
+];
