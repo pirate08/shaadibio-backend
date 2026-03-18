8a4860e71e31dc882e25c8ebefd41d62287ae8c4
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-18 20:10:44 +0530

    Single template function's route has been created...

--- src/routes/templateRoutes.js
+++ src/routes/templateRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { getAllTemplates } = require("../controllers/templateController");
+const {
+  getAllTemplates,
+  getSingleTemplate,
+} = require("../controllers/templateController");
 
 const router = express.Router();
 
 // --Get all templates route--
 router.get("/", protect, getAllTemplates);
+
+// --Get single template route--
+router.get("/:id", protect, getSingleTemplate);
 
 module.exports = router;
