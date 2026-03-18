763752c694666a86aae3011e2d3edbfccca6e956
Mayukh <mayukhdebgoswami82@gmail.com>
2026-03-18 19:57:35 +0530

    Get all template function has been created...

--- src/controllers/templateController.js
+++ src/controllers/templateController.js
@@ -1,3 +1,20 @@
 const asyncHandler = require("express-async-handler");
 const Biodata = require("../models/BioData");
 const Template = require("../models/Template");
+
+// --Get all templates--
+const getAllTemplates = asyncHandler(async (req, res) => {
+  const template = await Template.find({ isActive: true });
+  if (!template) {
+    res.status(404);
+    throw new Error("No template found");
+  }
+  res.status(200).json({
+    success: true,
+    message: "All templates fetched.",
+    count: template.length,
+    data: template,
+  });
+});
+
+module.exports = { getAllTemplates };
